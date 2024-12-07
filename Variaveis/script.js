54e941cf67b7ffb3a396b284340c034f1cf392a8
Gabriel <azambujagabriel95@gmail.com>
2024-12-07 13:01:21 -0300

    [feat]Variáveis de teste

---
+++ Variaveis/script.js
@@ -0,0 +1,33 @@
+// var nome = 'Gabriel';
+// var idade = 27;
+// var possuiFaculdade = true;
+
+// console.log(nome, idade, possuiFaculdade);
+
+// var preco = 25;
+// var totalComprado = 5;
+// var totalPreco = totalComprado * preco;
+
+// console.log(totalPreco);
+
+// var sobrenome = 'Rafael',
+//   cidade = 'Rio';
+
+// console.log(sobrenome, cidade);
+// // console.log(aindaNaoDefini);
+
+// var comida;
+// comida = 'Pizza';
+// console.log(comida);
+
+// var time = 'Vasco';
+// console.log(time);
+// time = 'Flamengo';
+// console.log(time);
+
+var nome = 'Gabriel';
+var idade = 27;
+var comidaFavorita;
+comidaFavorita = 'Beirute';
+var hobby, musicaFavorita, serieFavorita, filmeFavorito, ganeroDeFilmeFavorito;
+console.log(nome, idade, comidaFavorita);

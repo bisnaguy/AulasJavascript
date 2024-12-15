064c9fa9c5042acc04ff60ed546445426f3fd500
Gabriel <azambujagabriel95@gmail.com>
2024-12-15 12:52:11 -0300

    [fix]Arrumando as pastas e adicionando operadores lógicos

---
+++ Operadores lógicos/script.js
@@ -0,0 +1,7 @@
+console.log(5 > 3 && 3 == 2);
+console.log(5 > 3 || 3 == 1);
+
+console.log(5 == 3 && 'Gabriel' == 'Gabriel');
+console.log('Gabriel' == 'João' || false);
+
+console.log(!(true && true));
--- Testes/script.js
+++ Testes/script.js
@@ -1,3 +1,9 @@
 console.log('Quebra de linha \n na String');
 console.log('Meu nome é "Gabriel"');
 console.log(`Usando cálculos na String ${5 * 3}`);
+
+if (5 > 3) {
+  console.log('É maior');
+} else {
+  console.log('É menor');
+}

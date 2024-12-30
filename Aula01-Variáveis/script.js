be6e19f8580528e30f18c946b85fb8d3abc2f10e
Gabriel <azambujagabriel95@gmail.com>
2024-12-30 10:22:32 -0300

    [fix-feat]Correção dos nomes dos exercícios e adicionando aula01 variáveis

---
+++ Aula01-Variáveis/script.js
@@ -0,0 +1,25 @@
+let nome = 'Gabriel';
+
+console.log(nome);
+console.log(`O meu nome é ${nome}`);
+
+let laranja = 5;
+console.log(laranja * laranja);
+
+nome = 'Alfredo';
+
+console.log(nome);
+
+laranja = 45;
+
+console.log(laranja);
+
+laranja = 'laranja';
+
+console.log(laranja);
+
+let um = 1,
+  dois = 2,
+  tres = 3;
+
+console.log(um + dois + tres);

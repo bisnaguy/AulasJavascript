1f668ff47b5896175dea085c8d5f0a7ab88246cb
Gabriel <azambujagabriel95@gmail.com>
2025-01-05 14:46:04 -0300

    [feat]Adicionando mais aulas

---
+++ 07-Math/script.js
@@ -0,0 +1,11 @@
+let maior = Math.max(5, 20, 42, 8);
+console.log(maior);
+
+let menor = Math.min(4, 2, 56, 8);
+console.log(menor);
+
+let arredondar = Math.round(5.469);
+console.log(arredondar);
+
+let arredondarParaCima = Math.ceil(5.469);
+console.log(arredondarParaCima);
---
+++ 08-Console.log/script.js
@@ -0,0 +1,6 @@
+let idade = 27;
+let nome = 'Gabriel';
+
+console.log(idade);
+console.log(nome);
+console.log(`O meu nome é ${nome} e tenhom ${idade} anos`);

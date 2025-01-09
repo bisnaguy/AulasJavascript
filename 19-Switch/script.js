67b9235d6b507051b1a9a8c0b0f22f7402ca290f
Gabriel <azambujagabriel95@gmail.com>
2025-01-09 13:25:38 -0300

    [feat]Adicionando break, continue e switch case

---
+++ 19-Switch/script.js
@@ -0,0 +1,14 @@
+let nome = prompt('Qual é o seu nome? ');
+
+switch (nome) {
+  case 'Gabriel':
+    console.log('Seu nome é Gabriel');
+    break;
+
+  case 'João':
+    console.log('Seu nome é João');
+
+  default:
+    console.log('O nome não foi encontrado');
+    break;
+}

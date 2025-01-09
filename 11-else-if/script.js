4b76c2aa95ce122626b6e0f234ca6603b16d6952
Gabriel <azambujagabriel95@gmail.com>
2025-01-09 01:32:26 -0300

    [feat] Utilizando  condicionais

---
+++ 11-else-if/script.js
@@ -0,0 +1,16 @@
+let nome = 'Gabriel';
+let idade = 27;
+
+if (nome != undefined && nome == 'Joaquim') {
+  console.log('Nome definido');
+} else if (nome == 'Gabriel' && nome.length > 5) {
+  console.log('O nome é Gabriel');
+} else {
+  console.log('Não é Gabriel');
+}
+
+if (1 > 2) {
+  console.log('Teste 1');
+} else {
+  console.log('Teste 2 validado');
+}

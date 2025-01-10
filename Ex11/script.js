278becab1653b868a7eaaf102a6b3870f0c6ceff
Gabriel <azambujagabriel95@gmail.com>
2025-01-10 08:28:38 -0300

    [feat]Adicionando mais exercícios

---
+++ Ex11/script.js
@@ -0,0 +1,10 @@
+let cnh = true;
+let idade = 18;
+
+if (cnh == true && idade >= 18) {
+  console.log('Você pode dirigir');
+} else if (cnh == false && idade >= 18) {
+  console.log('Você não tem CNH para dirigir');
+} else {
+  console.log('Você é menor de idade');
+}
---
+++ Ex12/script.js
@@ -0,0 +1,6 @@
+let posicao = 0;
+
+while (posicao <= 10) {
+  console.log(`A posição atual é ${posicao}`);
+  posicao = posicao + 1;
+}
